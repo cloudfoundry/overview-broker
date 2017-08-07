062e08d2977350ca89078dc4757d6cf779dbc335
Matt McNeeney <mmcneeney@pivotal.io>
2017-08-07 09:36:48 +0100

    fix schema

--- service_broker.js
+++ service_broker.js
@@ -88,6 +88,7 @@ class ServiceBroker {
         var complexPlanSchema = {
             $schema: 'http://json-schema.org/draft-04/schema#',
             additionalProperties: false,
+            type: 'object',
             properties: {
                 rainbow: {
                     type: 'boolean',
